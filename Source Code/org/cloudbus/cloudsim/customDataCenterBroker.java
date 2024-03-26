ddb15b3c4f5667e1636dde1c033ead41f3f7cf4f
Akansh Vaibhav <94189081+akanshv@users.noreply.github.com>
2024-03-26 13:31:32 +0530

    Add files via upload

---
+++ Source Code/org/cloudbus/cloudsim/customDataCenterBroker.java
@@ -0,0 +1,13 @@
+package org.cloudbus.cloudsim;
+import org.cloudbus.cloudsim.DatacenterBroker;
+public class customDataCenterBroker extends DatacenterBroker {
+
+	public customDataCenterBroker(String name) throws Exception {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	   public void scheduleTaskstoVms() {
+		   
+	   }
+
+}
